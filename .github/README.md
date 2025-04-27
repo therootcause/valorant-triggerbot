52732de950b042d2876f32c5a2527a5322d2aab6
lencydev <lencydev@gmail.com>
2025-04-27 21:49:39 +0400

    chore(release): v1.4.0

---
+++ .github/README.md
@@ -0,0 +1,64 @@
+<div align = "center">
+  <img src = "../assets/header.png" alt = "header" />
+  <br />
+  <a href = "https://github.com/lencydev/valorant-triggerbot/releases/latest"><img src = "https://img.shields.io/github/v/release/lencydev/valorant-triggerbot?style=flat-square&color=5864F9" alt = "Release" /></a>
+  <a href = "https://github.com/lencydev/valorant-triggerbot/blob/main/LICENSE"><img src = "https://img.shields.io/github/license/lencydev/valorant-triggerbot?style=flat-square&color=5864F9" alt = "License" /></a>
+  <a href = "https://github.com/lencydev/valorant-triggerbot/releases"><img src = "https://img.shields.io/github/downloads/lencydev/valorant-triggerbot/total?style=flat-square&color=5864F9" alt = "Downloads" /></a>
+  <a href = "https://github.com/lencydev/valorant-triggerbot/forks?include=active,archived,inactive,network&page=1&period=&sort_by=last_updated"><img src = "https://img.shields.io/github/forks/lencydev/valorant-triggerbot?style=flat-square&color=5864F9" alt = "Forks" /></a>
+  <a href = "https://github.com/lencydev/valorant-triggerbot/stargazers"><img src = "https://img.shields.io/github/stars/lencydev/valorant-triggerbot?style=flat-square&color=5864F9" alt = "Stars" /></a>
+  <br />
+  <a href = "https://ko-fi.com/lencydev"><img src = "https://img.shields.io/badge/Supports%20on%20Ko--fi-FF6433?logo=ko-fi&logoColor=FFF&style=flat-square" alt = "Ko-fi" /></a>
+</div>
+
+# Usage
+1. Download the latest release from the [releases](https://github.com/lencydev/valorant-triggerbot/releases) page.
+2. Run the exe file and click the `Enable` button to enable the triggerbot.
+
+### Valorant Settings
+These are the in-game settings required for the triggerbot to work.
+- `General > Accessibility > Enemy Highlight Color:` Select `Purple`.
+- `Controls > Equipment > Weapons > Fire:` Set the secondary key to `K`.
+
+### Default Settings
+These are the default and recommended settings.
+- `Resolution:` 1920x1080 (your in-game resolution is different, change)
+- `Trigger Keys:` Left Shift
+- `Trigger Delay:` 50ms
+- `Trigger Area:` 5.0
+- `Target Color:` Purple (R 240 G 90 B 255)
+- `Color Tolerance:` 50
+
+# Building from Source
+If you prefer to build the application yourself, follow these steps:
+
+1. Ensure you have [Rust](https://www.rust-lang.org/tools/install) installed on your system.
+2. Clone the repository:
+   ```bash
+   git clone https://github.com/lencydev/valorant-triggerbot.git
+   cd valorant-triggerbot
+   ```
+3. Build the project:
+   ```bash
+   cargo build --release
+   ```
+4. After the compilation:
+   - Navigate to the `target/release` directory.
+   - Find the `valorant-triggerbot.exe` file.
+   - Run exe to start the application.
+
+# Disclaimer
+> [!WARNING]
+> Currently, this application is not detectable by Vanguard.<br />
+> However, this may change in the future as Riot Games continuously updates their security measures.
+
+- The author is not responsible for any consequences resulting from the use of this software.
+- Using this application violates the game's terms of service and may result in your account being banned.
+
+# Support
+If you need help, you can message me on [discord](https://discord.com/users/313738210729656332).
+
+<hr />
+
+<div align = "center">
+  <video src = "https://github.com/user-attachments/assets/c2a3a180-24ae-4ed2-9c6d-b27732631dc2" />
+</div>
---
+++ src/gui.rs
@@ -0,0 +1,184 @@
+use crate::app::Triggerbot;
+
+use eframe::egui::{
+  Button, CentralPanel, Color32, ComboBox, Context, DragValue, Frame, RichText, ScrollArea, Slider,
+  Stroke, Vec2,
+};
+
+pub fn build(app: &mut Triggerbot, ctx: &Context) {
+  CentralPanel::default().show(ctx, |ui| {
+    ui.vertical(|ui| {
+      ui.set_width(ui.available_width());
+
+      ui.group(|ui| {
+        ui.set_width(ui.available_width());
+
+        ui.horizontal(|ui| {
+          let mut width = app.settings.resolution.width;
+          let mut height = app.settings.resolution.height;
+
+          ui.label("Resolution:");
+          if ui
+            .add(DragValue::new(&mut width).speed(1).range(1..=5000))
+            .changed()
+          {
+            app.set_resolution(width, app.settings.resolution.height);
+          }
+          ui.label("x");
+          if ui
+            .add(DragValue::new(&mut height).speed(1).range(1..=5000))
+            .changed()
+          {
+            app.set_resolution(app.settings.resolution.width, height);
+          }
+
+          ui.checkbox(&mut app.settings.always_open, "Always Open");
+        });
+
+        ui.add_space(5.0);
+
+        ui.horizontal(|ui| {
+          ui.set_width(ui.available_width());
+
+          ui.label("Trigger Keys:");
+
+          ComboBox::from_id_salt("combobox_trigger_keys")
+            .selected_text(format!("{} keys selected", app.settings.trigger_keys.len()))
+            .width(160.0)
+            .show_ui(ui, |ui| {
+              ui.set_min_width(160.0);
+              ui.set_max_height(80.0);
+
+              ScrollArea::vertical().max_height(200.0).show(ui, |ui| {
+                let available_triggers = app.get_keys();
+
+                for trigger in available_triggers {
+                  let mut is_selected = app.settings.trigger_keys.contains(&trigger);
+                  let display_name = app.get_keys_display_name(&trigger);
+
+                  if ui.checkbox(&mut is_selected, display_name).changed() {
+                    if is_selected && !app.settings.trigger_keys.contains(&trigger) {
+                      app.settings.trigger_keys.push(trigger);
+                    } else if !is_selected {
+                      if let Some(pos) =
+                        app.settings.trigger_keys.iter().position(|x| *x == trigger)
+                      {
+                        app.settings.trigger_keys.remove(pos);
+                      }
+                    }
+                  }
+                }
+              });
+            });
+        });
+
+        ui.add_space(5.0);
+
+        ui.horizontal(|ui| {
+          ui.set_width(ui.available_width());
+
+          ui.label("Trigger Delay (ms):");
+          ui.add(Slider::new(&mut app.settings.trigger_delay, 0..=3000));
+        });
+
+        ui.add_space(5.0);
+
+        ui.horizontal(|ui| {
+          ui.set_width(ui.available_width());
+
+          ui.label("Trigger Area:");
+          if ui
+            .add(Slider::new(&mut app.settings.trigger_area, 5.0..=100.0))
+            .changed()
+          {
+            app.update_trigger_area();
+          }
+        });
+
+        ui.add_space(5.0);
+
+        ui.horizontal(|ui| {
+          ui.set_width(ui.available_width());
+
+          ui.label("Target Color:");
+
+          let labels = ["R", "G", "B"];
+
+          for i in 0..3 {
+            ui.label(labels[i]);
+            ui.add(
+              DragValue::new(&mut app.settings.target_color[i])
+                .speed(1)
+                .range(0..=255),
+            );
+          }
+
+          Frame::none()
+            .fill(Color32::from_rgb(
+              app.settings.target_color[0] as u8,
+              app.settings.target_color[1] as u8,
+              app.settings.target_color[2] as u8,
+            ))
+            .stroke(Stroke::new(1.0, Color32::WHITE))
+            .inner_margin(Vec2::new(6.0, 6.0))
+            .show(ui, |_| {});
+        });
+
+        ui.add_space(5.0);
+
+        ui.horizontal(|ui| {
+          ui.set_width(ui.available_width());
+
+          ui.label("Color Tolerance:");
+          ui.add(Slider::new(&mut app.settings.color_tolerance, 0..=255));
+        });
+      });
+
+      ui.add_space(5.0);
+
+      ui.horizontal(|ui| {
+        ui.set_width(ui.available_width());
+
+        let (button_text, button_color) = if app.enabled {
+          ("Disable", Color32::from_rgb(180, 100, 100))
+        } else {
+          ("Enable", Color32::from_rgb(100, 180, 100))
+        };
+
+        if ui
+          .add_sized(
+            [ui.available_width() / 2.0, 30.0],
+            Button::new(RichText::new(button_text).color(Color32::WHITE)).fill(button_color),
+          )
+          .clicked()
+        {
+          app.enabled = !app.enabled;
+        };
+
+        let is_default = app.is_default_settings();
+
+        let reset_button_color = if is_default {
+          Color32::DARK_GRAY
+        } else {
+          Color32::from_rgb(100, 100, 180)
+        };
+
+        let reset_button = Button::new(RichText::new("Reset Settings").color(Color32::WHITE))
+          .fill(reset_button_color)
+          .sense(if is_default {
+            eframe::egui::Sense::hover()
+          } else {
+            eframe::egui::Sense::click()
+          });
+
+        if ui
+          .add_sized([ui.available_width(), 30.0], reset_button)
+          .clicked()
+          && !is_default
+        {
+          app.reset_settings();
+        }
+      });
+    });
+  });
+}
